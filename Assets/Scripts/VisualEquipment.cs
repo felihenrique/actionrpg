912a28fa402f62de6df1af7fd8f8bf220b2bff08
felihenrique <felihenrique@gmail.com>
2016-11-20 17:01:27 -0300

    .

---
+++ Assets/Scripts/VisualEquipment.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualEquipment : MonoBehaviour {
+
+	// Use this for initialization
+	public Texture2D slash_base;
+	public Texture2D weapon;
+	public Texture2D slash_inuse;
+	void Start () {
+		StartCoroutine (EditTexture ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator EditTexture() {
+		slash_inuse.SetPixels (slash_inuse.AlphaBlend (weapon).GetPixels ());
+		slash_inuse.Apply ();
+		yield return new WaitForEndOfFrame ();
+	}
+}
