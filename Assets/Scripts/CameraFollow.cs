a6b7dc6ce469bb3125201e8d3b56bda7cd221a51
felihenrique <felihenrique@gmail.com>
2016-12-02 21:37:57 -0300

    Melhorado script camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,14 +12,30 @@ public class CameraFollow : MonoBehaviour {
 	void Start () {
 		transform = GetComponent<Transform> ();
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-		Vector3 pos = entityToFollow.position;
-		pos.z = transform.position.z;
-		transform.position = Vector3.Lerp (transform.position, pos, Time.deltaTime * speed);
-		if (Vector3.Distance(transform.position, pos) < tolerance) {
-			transform.position = pos;
+
+	void Update()
+	{
+		if (entityToFollow != null)
+		{
+			float player_x = entityToFollow.position.x;
+			float player_y = entityToFollow.position.y;
+
+			float rounded_x = RoundToNearestPixel(player_x);
+			float rounded_y = RoundToNearestPixel(player_y);
+
+			Vector3 new_pos = new Vector3(rounded_x, rounded_y, -10.0f); // this is 2d, so my camera is that far from the screen.
+			Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new_pos, 0.16f);
 		}
 	}
+	public float pixelToUnits = 128f;
+
+	public float RoundToNearestPixel(float unityUnits)
+	{
+		float valueInPixels = unityUnits * pixelToUnits;
+		valueInPixels = Mathf.Round(valueInPixels);
+		float roundedUnityUnits = valueInPixels * (1 / pixelToUnits);
+		return roundedUnityUnits;
+	}
+
+
 }
