c727725fe04f673f8f711d1f81588759de5a1319
felihenrique <felihenrique@gmail.com>
2018-02-03 04:17:54 -0200

    Adicionado janela básica para criação de itens

---
+++ Assets/Editor/CreateItemDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateItemDialog : EditorWindow {
+    Sprite spr = null;
+    string name = "";
+    bool result = false;
+
+    [MenuItem("RPG/Itens/Create Item")]
+    static void Init() {
+        CreateItemDialog window = (CreateItemDialog)EditorWindow.GetWindow<CreateItemDialog>("Create Item");
+        window.minSize = new Vector2(400, 150);
+        window.Show();
+    }
+
+    void OnGUI() {
+        spr = (Sprite)EditorGUILayout.ObjectField("Sprite: ", spr, typeof(Sprite), false);
+        name = EditorGUILayout.TextField("Name: ", name);
+        result = GUILayout.Button("Confirm");
+        if (result)
+        {
+            GameObject obj = new GameObject(name);
+            obj.AddComponent<Item>();
+            obj.GetComponent<Item>().itemName = name;
+            obj.GetComponent<Item>().icon = spr;
+            obj.GetComponent<SpriteRenderer>().sprite = spr;
+            PrefabUtility.CreatePrefab("Assets/Prefabs/Itens/" + name + ".prefab", obj);
+        }
+    }
+}
